97192bed5ccbe3ba96ecc8a919a51dea319c2695
oasis nini <oasisnini@gmail.com>
2016-06-12 21:31:25 +0900

    name change

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,7 +26,7 @@
           <ul class="nav navbar-nav">
             <li class="active"><a href="/">Home</a></li>
             <li><a href="/flyersIndex">street</a></li>
-            <li><a href="/3310811/Mineki Sekiuchi">Mineki Sekiguchi</a></li>
+            <li><a href="/3310811/Mineki Sekiguchi">Mineki Sekiguchi</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
